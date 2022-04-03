4468218b0eab472b63eedead4a178528eddcafb7
jayant <jayantparmar420@gmail.com>
2022-04-03 14:29:47 +0530

    Added: Happy Or Sad Case in Mood Analyzer.

---
+++ src/main/java/MoodAnalyserMain.java
@@ -0,0 +1,24 @@
+/**
+ * Purpose  -  ability to analyse and respond Happy or Sad Mood
+ * @author  - jayant singh parmar
+ * @version - 16.0
+ * @since   - 03-04-2022
+ */
+
+/**
+ * create a class name as MoodAnalyserMain
+ */
+public class MoodAnalyserMain {
+    /**
+     * create a constructor ,this is default constructor name as MoodAnalyserMain
+     */
+    public MoodAnalyserMain() {
+    }
+
+    public String analyseMood(String message) {
+        if (message.contains("Sad"))
+            return "SAD";
+        else
+            return "HAPPY";
+    }
+}
