a70439f66a35380c577aa14800ff5ccf1dfbc260
jayant <jayantparmar420@gmail.com>
2022-04-03 14:40:31 +0530

    Added:  Refactor Happy Or Sad Case in Mood Analyzer.

--- src/main/java/MoodAnalyserMain.java
+++ src/main/java/MoodAnalyserMain.java
@@ -1,6 +1,7 @@
-/**
- * Purpose  -  ability to analyse and respond Happy or Sad Mood
- * @author  - jayant singh parmar
+
+ /**
+ * Purpose  - Refactor the code to take the mood message in Constructor
+ * @author  - Jayant singh parmar
  * @version - 16.0
  * @since   - 03-04-2022
  */
@@ -9,14 +10,18 @@
  * create a class name as MoodAnalyserMain
  */
 public class MoodAnalyserMain {
+    private String message;
     /**
      * create a constructor ,this is default constructor name as MoodAnalyserMain
      */
     public MoodAnalyserMain() {
     }
 
-    public String analyseMood(String message) {
-        if (message.contains("Sad"))
+    public MoodAnalyserMain(String message) {
+        this.message=message;
+    }
+    public String analyseMood() {
+        if (this.message.contains("Sad"))
             return "SAD";
         else
             return "HAPPY";
